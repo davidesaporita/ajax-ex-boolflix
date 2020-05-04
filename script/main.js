27b0b9603c884366a32523366c4f195082eb032a
Davide Saporita <davide.saporita@gmail.com>
2020-05-04 12:35:09 +0200

    API call working
    Tested using console.log

--- script/main.js
+++ script/main.js
@@ -6,7 +6,42 @@
 
 $(document).ready(function() {
 
+    // Vars
+    var apiKey = '2c7968616e24faf12903bba4628706b2';
+    var language = 'it-IT';
+    
+    // Refs
+    var searchInput = $('#search-input');
+    var searchBtn = $('#search-button');
+    var list = $('.result-list');
 
-
+    searchBtn.click(function() {
+        var query = searchInput.val();
+        $.ajax({
+            url: 'https://api.themoviedb.org/3/search/movie', 
+            method: 'GET',
+            data: {
+                api_key: apiKey,
+                language: language,
+                query: query
+            },
+            success: function(data){
+                data.results.forEach(element => {
+                    console.log(element.title);
+                    console.log(element.original_title);
+                    console.log(element.original_language);
+                    console.log(element.vote_average);    
+                });
+            },
+            error: function() {
+                console.log('Qualcosa non funziona');
+            }
+        });
+    });
     
 }); // End of ready function
+
+/*  ------------
+   | Functions |
+   ------------   */
+
