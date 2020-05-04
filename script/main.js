6e9d33df4547ca9de44acb38e366c2fe38426241
Davide Saporita <davide.saporita@gmail.com>
2020-05-04 13:25:59 +0200

    Added keyup event (Enter) on search input + Refactoring

--- script/main.js
+++ script/main.js
@@ -5,15 +5,11 @@
  */
 
 $(document).ready(function() {
-
-    // Vars
-    var apiKey = '2c7968616e24faf12903bba4628706b2';
-    var language = 'it-IT';
-    
+   
     // Refs
     var searchInput = $('#search-input');
     var searchBtn = $('#search-button');
-    var list = $('.result-list');
+    var app = $('#app');
 
     // Init Handlebars
     var source = $('#result-template').html();
@@ -21,30 +17,18 @@ $(document).ready(function() {
 
     searchBtn.click(function() {
         var query = searchInput.val();
-        $.ajax({
-            url: 'https://api.themoviedb.org/3/search/movie', 
-            method: 'GET',
-            data: {
-                api_key: apiKey,
-                language: language,
-                query: query
-            },
-            success: function(data) {
-                list.html('');
-                data.results.forEach(element => {
-                    var templateData = {
-                        title: element.title,
-                        originalTitle: element.original_title,
-                        originalLanguage: element.original_language,
-                        voteAverage: element.vote_average
-                    }
-                    list.append(template(templateData));
-                });
-            },
-            error: function() {
-                console.log('Qualcosa non funziona');
+        if(query.length > 2) {
+            movieSearch(query, template);
+        }
+    });
+
+    app.on('keyup','#search-input', (e) => {
+        if(e.which === 13 || e.keyCode === 13) {
+            var query = searchInput.val();
+            if(query.length > 2) {
+                movieSearch(query, template);
             }
-        });
+        }
     });
     
 }); // End of ready function
@@ -53,3 +37,37 @@ $(document).ready(function() {
    | Functions |
    ------------   */
 
+function movieSearch(query, template) {
+    // Vars
+    var apiUrl = 'https://api.themoviedb.org/3/search/movie';
+    var apiKey = '2c7968616e24faf12903bba4628706b2';
+    var language = 'it-IT';
+
+    // Refs
+    var list = $('.result-list');
+
+    $.ajax({
+        url: apiUrl, 
+        method: 'GET',
+        data: {
+            api_key: apiKey,
+            language: language,
+            query: query
+        },
+        success: function(data) {
+            list.html('');
+            data.results.forEach(element => {
+                var templateData = {
+                    title:            element.title,
+                    originalTitle:    element.original_title,
+                    originalLanguage: element.original_language,
+                    voteAverage:      element.vote_average
+                }
+                list.append(template(templateData));
+            });
+        },
+        error: function() {
+            console.log('Qualcosa non funziona');
+        }
+    });
+}
