10d973233bdd106602efedfb2c49169ba8eb682f
Davide Saporita <davide.saporita@gmail.com>
2020-05-05 11:26:14 +0200

    Stars rating added

--- script/main.js
+++ script/main.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
     searchBtn.click(function() {
         var query = searchInput.val();
         if(query.length > 2) {
-            movieSearch(query, template);
+            apiCall(query, template);
         }
     });
 
@@ -26,7 +26,7 @@ $(document).ready(function() {
         if(e.which === 13 || e.keyCode === 13) {
             var query = searchInput.val();
             if(query.length > 2) {
-                movieSearch(query, template);
+                apiCall(query, template);
             }
         }
     });
@@ -37,7 +37,7 @@ $(document).ready(function() {
    | Functions |
    ------------   */
 
-function movieSearch(query, template) {
+function apiCall(query, template) {
     // Vars
     var apiUrl = 'https://api.themoviedb.org/3/search/movie';
     var apiKey = '2c7968616e24faf12903bba4628706b2';
@@ -56,18 +56,44 @@ function movieSearch(query, template) {
         },
         success: function(data) {
             list.html('');
-            data.results.forEach(element => {
-                var templateData = {
-                    title:            element.title,
-                    originalTitle:    element.original_title,
-                    originalLanguage: element.original_language,
-                    voteAverage:      element.vote_average
-                }
-                list.append(template(templateData));
-            });
+            if(data.results.length > 0){
+                data.results.forEach(element => {
+                    var templateData = {
+                        title:            element.title,
+                        originalTitle:    element.original_title,
+                        originalLanguage: element.original_language,
+                        voteAverage:      element.vote_average,
+                        stars:            howManyStars(element.vote_average)
+                    }
+
+                    list.append(template(templateData));
+                });
+            } else {
+                alert('Nessun film trovato');
+                //searchInput.select();
+            }
         },
         error: function() {
             console.log('Qualcosa non funziona');
+            //searchInput.focus();
         }
     });
+}
+
+function howManyStars(vote) {
+    // Vars
+    var starFull = '<i class="fas fa-star"></i>';
+    var starEmpty = '<i class="far fa-star"></i>';
+    var html = '';
+
+    // Modifica scala di valori e arrotonamento per eccesso
+    var starsVote = Math.ceil(vote*0.5);
+
+    // Ciclo per generare html
+    for(var i = 0; i < 5; i++) {
+        if(starsVote > i) html += starFull;
+        else              html += starEmpty;
+    }
+
+    return html;
 }
